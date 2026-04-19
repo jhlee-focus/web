2f0f2e069306e388c065b60e0282b30e9a23a134
jhlee-focus <jhlee.focus@gmail.com>
2026-04-20 07:50:29 +0900

    Add files via upload

---
+++ avengers_game/퍼즐 이미지/images.js
@@ -0,0 +1,9 @@
+// 🧩 슬라이딩 퍼즐 - 이미지 목록
+// 이 파일은 update-images.bat 을 실행하면 자동으로 갱신됩니다.
+window.PUZZLE_IMAGES = [
+  "앤트맨 vs 레비아탄.webp",
+  "어벤져스 어셈블.jpg",
+  "어벤져스 영화 단체 이미지.png",
+  "어벤져스 팀.webp",
+  "타노스 팀.webp"
+];
